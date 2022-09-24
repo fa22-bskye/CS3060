eb0c83e05744cdbdfb66db85e826a00dfb7643f1
bskye <skye.bielstein@gmail.com>
2022-09-23 22:14:46 -0400

    Made comments to task2 in hw1

--- hw1/task2.rb
+++ hw1/task2.rb
@@ -7,7 +7,7 @@ Ruby = "Ruby"
 openFile = File.open(theFile, "r")
 
 openFile.each do |line|
-    if f = line.scan( /\b#{Python}\b/i )
+    if f = line.scan( /\b#{Python}\b/i ) #You can also do quotes as well, str interpol.
         puts "The file is important"
     elsif j = line.scan( /\b#{Ruby}\b/i)
         puts "The file is important"
